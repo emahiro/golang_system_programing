49504f04664c06deba00ee7fbb5161c0897cd4fb
emahiro <hiromichi.a.ema@dena.com>
2018-01-06 03:30:01 +0900

    add text write process

--- src/main.go
+++ src/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"hash/crc32"
 	"io"
 	"os"
 )
@@ -35,7 +37,7 @@ func readChunks(file *os.File) []io.Reader {
 			break
 		}
 
-		// 最初のchunkから12(3 * 4byte)byteずらす
+		// 最初のchunk(4byte)から12(3 * 4byte)byte含めた16byteが1chunk
 		chunks = append(chunks, io.NewSectionReader(file, ofs, int64(l)+12))
 
 		// 次のchunkの先頭に移動
@@ -46,7 +48,20 @@ func readChunks(file *os.File) []io.Reader {
 	return chunks
 }
 
-func main() {
+func textChunk(txt string) io.Reader {
+	byteData := []byte(txt)
+	var buf bytes.Buffer
+	binary.Write(&buf, binary.BigEndian, int32(len(byteData))) // 書き込むためのbufferを確保。長さは入力文字長。
+	buf.WriteString("tExt")                                    //bufferに書き込む
+	buf.Write(byteData)                                        // 入力対象の文字をbufferに書き込む
+	// CRCを計算して追加
+	crc := crc32.NewIEEE()
+	io.WriteString(crc, "tExt")
+	binary.Write(&buf, binary.BigEndian, crc.Sum32()) // crc分を新しくbufferに書き込む
+	return &buf                                       // 書き込み終わったbufferを返す
+}
+
+func WriteNewTextChunk() {
 	// pngをチャンクに分けて読み込み
 	file, err := os.Open("./imgs/Lenna.png")
 	if err != nil {
@@ -54,6 +69,35 @@ func main() {
 		os.Exit(-1)
 	}
 	defer file.Close()
+
+	newFile, _ := os.Create("./imgs/result.png")
+	defer newFile.Close()
+
+	chunks := readChunks(file)
+	// pngのシグニチャー書き込み
+	io.WriteString(newFile, "\x89PNG\r\n\x1a\n")
+
+	// 先頭にIHDR chunkを書き込む
+	io.Copy(newFile, chunks[0])
+
+	// TextChunk を追加する
+	io.Copy(newFile, textChunk("Test Text Chunk"))
+
+	// 残りのチャンクを新しく追加する
+	for _, c := range chunks[1:] {
+		io.Copy(newFile, c)
+	}
+}
+
+func main() {
+	// 私いchunkを書き込んだfileを生成する
+	WriteNewTextChunk()
+
+	file, err := os.Open("./imgs/result.png")
+	if err != nil {
+		fmt.Printf("error! err: %v", err)
+		os.Exit(-1)
+	}
 
 	chunks := readChunks(file)
 	for _, c := range chunks {
